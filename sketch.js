acf8650d8c72df323b5680b0e918576f8b0b8e47
Eduds2341 <95101439+Eduds2341@users.noreply.github.com>
2021-11-26 16:01:00 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background(30);
+  textAlign(CENTER,CENTER);
+  textSize(18);
+  fill("#D3D3D3");
+text("olá mundo",200,200);
+if (keyDown("space")) {
+  background("#6959CD")
+}
+}
+
+
+
+
+ 
